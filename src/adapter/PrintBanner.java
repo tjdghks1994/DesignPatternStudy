a55402a464567cd12ee0b3316c43ae226d7819fa
SungHwan <tjdghks130907@gmail.com>
2023-11-16 23:41:02 +0900

    [Adapter패턴] 어댑터 역할을 하는 클래스

---
+++ src/adapter/PrintBanner.java
@@ -0,0 +1,19 @@
+package adapter;
+
+// 어댑터 역할을 하는 클래스
+public class PrintBanner extends Banner implements Print {
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeak() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        showWithAster();
+    }
+}
