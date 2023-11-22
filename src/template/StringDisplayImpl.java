4a3d18f89600dfe125fe7ed55b65234f87f0ada1
SungHwan <tjdghks130907@gmail.com>
2023-11-22 22:54:58 +0900

    [Template 패턴]
    - 인터페이스를 구현한 구현클래스
    - 인터페이스에 선언된 추상메서드를 구현
    - 구현클래스의 추상메서드는 인터페이스의 템플릿 메서드에서 호출

---
+++ src/template/StringDisplayImpl.java
@@ -0,0 +1,34 @@
+package template;
+
+public class StringDisplayImpl implements AbstractDisplayIF {
+    private String s;
+    private int width;
+
+    public StringDisplayImpl(String s) {
+        this.s = s;
+        this.width = s.length();
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + s + "|");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
