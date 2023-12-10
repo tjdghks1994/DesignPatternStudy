47abcad28b7f13d55717e1281f67866ab397d8fd
SungHwan <tjdghks130907@gmail.com>
2023-12-10 20:55:40 +0900

    [빌더 패턴]
    - Builder 역할의 인터페이스 선언

---
+++ src/builder/Builder.java
@@ -0,0 +1,11 @@
+package builder;
+
+public interface Builder {
+    public abstract void makeTitle(String title);
+
+    public abstract void makeString(String str);
+
+    public abstract void makeItems(String[] items);
+
+    public abstract void close();
+}
