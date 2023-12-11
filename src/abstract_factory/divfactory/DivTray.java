dd7b7dbcd7500eecc0f2ecb3135e8c65d4d4eac3
SungHwan <tjdghks130907@gmail.com>
2023-12-11 21:47:08 +0900

    [Abstract Factory 패턴]
    - 구체적인 제품 클래스
    - 각 역할에 알맞게 makeHTML() 메서드 구현

---
+++ src/abstract_factory/divfactory/DivTray.java
@@ -0,0 +1,25 @@
+package abstract_factory.divfactory;
+
+import abstract_factory.factory.Item;
+import abstract_factory.factory.Tray;
+
+public class DivTray extends Tray {
+    public DivTray(String caption) {
+        super(caption);
+    }
+
+    @Override
+    public String makeHTML() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<p><b>");
+        sb.append(caption);
+        sb.append("</b></p>\n");
+        sb.append("<div class=\"TRAY\">");
+        for (Item item : tray) {
+            sb.append(item.makeHTML());
+        }
+        sb.append("</div>\n");
+
+        return sb.toString();
+    }
+}
