e065bb26904e72074dd8cac12565734e0bc3a337
SungHwan <tjdghks130907@gmail.com>
2023-12-11 21:48:19 +0900

    [Abstract Factory 패턴]
    - 구체적인 공장 클래스
    - 구체적인 제품 클래스들의 인스턴스를 생성한다

---
+++ src/abstract_factory/divfactory/DivFactory.java
@@ -0,0 +1,24 @@
+package abstract_factory.divfactory;
+
+import abstract_factory.factory.Factory;
+import abstract_factory.factory.Link;
+import abstract_factory.factory.Page;
+import abstract_factory.factory.Tray;
+
+public class DivFactory extends Factory {
+    @Override
+    public Link createLink(String caption, String url) {
+        return new DivLink(caption, url);
+    }
+
+    @Override
+    public Tray createTray(String caption) {
+        return new DivTray(caption);
+    }
+
+    @Override
+    public Page createPage(String title, String author) {
+        return new DivPage(title, author);
+    }
+
+}
