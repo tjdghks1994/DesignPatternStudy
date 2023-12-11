16d6193c11d7b70e473a5dc6f3da2e5763e8c8ae
SungHwan <tjdghks130907@gmail.com>
2023-12-11 21:47:49 +0900

    [Abstract Factory 패턴]
    - 구체적인 제품 클래스
    - 각 역할에 알맞게 makeHTML() 메서드 구현

---
+++ src/abstract_factory/divfactory/DivLink.java
@@ -0,0 +1,15 @@
+package abstract_factory.divfactory;
+
+import abstract_factory.factory.Link;
+
+public class DivLink extends Link {
+    public DivLink(String caption, String url) {
+        super(caption, url);
+    }
+
+    @Override
+    public String makeHTML() {
+        return "<div class=\"LINK\"<a href=\"" + url + "\">" + caption + "</a></div>\n";
+    }
+
+}
