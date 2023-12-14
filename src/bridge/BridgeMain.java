205c7627c0482eb7ce986374a4e81cc39a288d1a
SungHwan <tjdghks130907@gmail.com>
2023-12-14 21:56:19 +0900

    [브릿지 패턴]
    - 기능 클래스 계층 타입으로 변수를 선언하고 생성자로 구현 클래스 계층의 인스턴스를 주입한다
    - 주입된 구현 클래스 계층의 인스턴스에 따라 실제 메소드 동작이 달라진다

---
+++ src/bridge/BridgeMain.java
@@ -0,0 +1,15 @@
+package bridge;
+
+public class BridgeMain {
+    public static void main(String[] args) {
+        Display d1 = new Display(new StringDisplayImpl("Hello Korea."));
+        Display d2 = new CountDisplay(new StringDisplayImpl("Hello World."));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello Universe."));
+        RandomCountDisplay d4 = new RandomCountDisplay(new StringDisplayImpl("Hello Random."));
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(5);
+        d4.randomDisplay(10);
+    }
+}
