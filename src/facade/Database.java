e45327e24f59b526f204f9f39311670e9c2c58ff
SungHwan <tjdghks130907@gmail.com>
2024-01-23 21:18:20 +0900

    [Facade 패턴]
    - 비즈니스 로직을 수행하는 서비스 클래스라고 생각하면 된다
    - 이메일 주소에서 사용자 이름을 얻는 클래스이다

---
+++ src/facade/Database.java
@@ -0,0 +1,19 @@
+package facade;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Database {
+    private Database() {}
+
+    // 데이터베이스 이름에서 Properties 를 얻는다
+    public static Properties getProperties(String dbname) throws IOException {
+        String filename = dbname + ".txt";
+        Properties prop = new Properties();
+        prop.load(new FileReader(filename));
+
+        return prop;
+    }
+}
