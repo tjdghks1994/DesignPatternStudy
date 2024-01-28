393ed54e4c13b44f3f282ff698a32853ae2836b5
SungHwan <tjdghks130907@gmail.com>
2024-01-28 22:07:50 +0900

    [Mediator 패턴]
    - Mediator 패턴은 각 멤버(Colleague)들은 항상 중재자를 통해서 행동하는 패턴이다
    - 멤버는 모두 중재자에게만 보고하고, 중재자만 멤버에게 지시를 내린다
    - 멤버를 컨트롤하는 로직은 중재자 안에서만 기술한다
    - 중재자 역할의 인터페이스 선언

---
+++ src/mediator/Mediator.java
@@ -0,0 +1,9 @@
+package mediator;
+
+public interface Mediator {
+    // Colleague를 생성한다
+    public abstract void createColleagues();
+
+    // Colleague의 상태가 변화했을 때 호출된다
+    public abstract void colleagueChanged();
+}
