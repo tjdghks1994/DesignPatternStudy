8634d1351e7237a1ab7c46dc7a04dc24628113a3
SungHwan <tjdghks130907@gmail.com>
2024-02-25 22:13:05 +0900

    [State 패턴]
    - 상태를 클래스로 표헌하는 패턴
    - 상태에 의존한 동작을 하는 메서드 모음을 가지고 있다

---
+++ src/state/State.java
@@ -0,0 +1,11 @@
+package state;
+
+public interface State {
+    void doClock(Context context, int hour);    // 시간 설정
+
+    void doUse(Context context);    // 금고 사용
+
+    void doAlarm(Context context);  // 비상벨
+
+    void doPhone(Context context);  // 일반 통화
+}
