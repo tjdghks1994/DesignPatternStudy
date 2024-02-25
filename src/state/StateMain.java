b2aea41907e049172f6da610e64a411dae8dacc7
SungHwan <tjdghks130907@gmail.com>
2024-02-25 22:19:22 +0900

    [State 패턴]
    - State 패턴을 활용한 예시를 실행하는 Main 클래스

---
+++ src/state/StateMain.java
@@ -0,0 +1,17 @@
+package state;
+
+public class StateMain {
+    public static void main(String[] args) {
+        SafeFrame frame = new SafeFrame("State Sample");
+        while (true) {
+            for (int hour = 0; hour < 24; hour++) {
+                frame.setClock(hour);   // 시간 설정
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
